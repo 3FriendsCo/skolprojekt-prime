5d8d70cc0b6934abd1e2920d4ccfee113a7c7d02
Nenne375 <neo.mannskar@gmail.com>
2023-04-27 00:09:11 +0200

    Prime Compiler

--- include/AST.h
+++ include/AST.h
@@ -50,7 +50,7 @@ struct AST_node {
 };
 
 namespace Unknown {
-    inline AST_node Unknown() {
+    inline AST_node Unknown(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
@@ -58,19 +58,19 @@ namespace Unknown {
 
 namespace Pre_processor {
     // PRE-PROCESSORS:
-    inline AST_node Seize() {
+    inline AST_node Seize(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Liberate() {
+    inline AST_node Liberate(std::vector<AST_node>* AST) {
         AST_node pre_process_liberate;
         return pre_process_liberate;
     }
-    inline AST_node Banish() {
+    inline AST_node Banish(std::vector<AST_node>* AST) {
         AST_node pre_process_banish;
         return pre_process_banish;
     }
-    inline AST_node Force() {
+    inline AST_node Force(std::vector<AST_node>* AST) {
         AST_node pre_process_force;
         return pre_process_force;
     }
@@ -79,50 +79,50 @@ namespace Pre_processor {
 namespace Primitive_type {
     namespace Numeric_type {
         // Integer:
-        inline AST_node byte()
+        inline AST_node byte(std::vector<AST_node>* AST)
         {
             AST_node pre_process_byte;
             return pre_process_byte;
         }
-        inline AST_node Short()
+        inline AST_node Short(std::vector<AST_node>* AST)
         {
             AST_node pre_process_short;
             return pre_process_short;
         }
-        inline AST_node Int()
+        inline AST_node Int(std::vector<AST_node>* AST)
         {
             AST_node pre_process_int;
             return pre_process_int;
         }
-        inline AST_node Long()
+        inline AST_node Long(std::vector<AST_node>* AST)
         {
             AST_node pre_process_long;
             return pre_process_long;
         }
         // Floating point:
-        inline AST_node Float()
+        inline AST_node Float(std::vector<AST_node>* AST)
         {
             AST_node pre_process_float;
             return pre_process_float;
         }
-        inline AST_node Double()
+        inline AST_node Double(std::vector<AST_node>* AST)
         {
             AST_node pre_process_double;
             return pre_process_double;
         }
     }
 
-    inline AST_node Boolean()
+    inline AST_node Boolean(std::vector<AST_node>* AST)
     {
         AST_node pre_process_boolean;
         return pre_process_boolean;
     }
-    inline AST_node Char()
+    inline AST_node Char(std::vector<AST_node>* AST)
     {
         AST_node pre_process_char;
         return pre_process_char;
     }
-    inline AST_node Void()
+    inline AST_node Void(std::vector<AST_node>* AST)
     {
         AST_node pre_process_void;
         return pre_process_void;
@@ -131,7 +131,7 @@ namespace Primitive_type {
 }
 
 namespace Nonprimitive_type {
-    inline AST_node String()
+    inline AST_node String(std::vector<AST_node>* AST)
     {
         AST_node pre_process_string;
         return pre_process_string;
@@ -139,17 +139,17 @@ namespace Nonprimitive_type {
 }
 
 namespace Conditional_statement {
-    inline AST_node If()
+    inline AST_node If(std::vector<AST_node>* AST)
     {
         AST_node pre_process_if;
         return pre_process_if;
     }
-    inline AST_node Else()
+    inline AST_node Else(std::vector<AST_node>* AST)
     {
         AST_node pre_process_else;
         return pre_process_else;
     }
-    inline AST_node Switch()
+    inline AST_node Switch(std::vector<AST_node>* AST)
     {
         AST_node pre_process_switch;
         return pre_process_switch;
@@ -157,17 +157,17 @@ namespace Conditional_statement {
 }
 
 namespace Loop {
-    inline AST_node While()
+    inline AST_node While(std::vector<AST_node>* AST)
     {
         AST_node pre_process_while;
         return pre_process_while;
     }
-    inline AST_node Loop()
+    inline AST_node Loop(std::vector<AST_node>* AST)
     {
         AST_node pre_process_loop;
         return pre_process_loop;
     }
-    inline AST_node For()
+    inline AST_node For(std::vector<AST_node>* AST)
     {
         AST_node pre_process_for;
         return pre_process_for;
@@ -175,12 +175,12 @@ namespace Loop {
 }
 
 namespace Control_flow_statement {
-    inline AST_node Continue()
+    inline AST_node Continue(std::vector<AST_node>* AST)
     {
         AST_node pre_process_continue;
         return pre_process_continue;
     }
-    inline AST_node Break()
+    inline AST_node Break(std::vector<AST_node>* AST)
     {
         AST_node pre_process_break;
         return pre_process_break;
@@ -188,32 +188,32 @@ namespace Control_flow_statement {
 }
 
 namespace IO {
-    inline AST_node Log()
+    inline AST_node Log(std::vector<AST_node>* AST)
     {
         AST_node pre_process_log;
         return pre_process_log;
     }
-    inline AST_node Formatted_Log()
+    inline AST_node Formatted_Log(std::vector<AST_node>* AST)
     {
         AST_node pre_process_formatted_log;
         return pre_process_formatted_log;
     }
-    inline AST_node Read()
+    inline AST_node Read(std::vector<AST_node>* AST)
     {
         AST_node pre_process_read;
         return pre_process_read;
     }
-    inline AST_node Formatted_Read()
+    inline AST_node Formatted_Read(std::vector<AST_node>* AST)
     {
         AST_node pre_process_formatted_read;
         return pre_process_formatted_read;
     }
-    inline AST_node Write()
+    inline AST_node Write(std::vector<AST_node>* AST)
     {
         AST_node pre_process_write;
         return pre_process_write;
     }
-    inline AST_node Formatted_Write()
+    inline AST_node Formatted_Write(std::vector<AST_node>* AST)
     {
         AST_node pre_process_formatted_write;
         return pre_process_formatted_write;
@@ -223,158 +223,158 @@ namespace IO {
 }
 
 namespace Symbol {
-    inline AST_node Dot()
+    inline AST_node Dot(std::vector<AST_node>* AST)
     {
         AST_node pre_process_dot;
         return pre_process_dot;
     }
-    inline AST_node Comma()
+    inline AST_node Comma(std::vector<AST_node>* AST)
     {
         AST_node pre_process_comma;
         return pre_process_comma;
     }
-    inline AST_node Colon()
+    inline AST_node Colon(std::vector<AST_node>* AST)
     {
         AST_node pre_process_colon;
         return pre_process_colon;
     }
-    inline AST_node Semicolon()
+    inline AST_node Semicolon(std::vector<AST_node>* AST)
     {
         AST_node pre_process_semicolon;
         return pre_process_semicolon;
     }
-    inline AST_node Questionmark()
+    inline AST_node Questionmark(std::vector<AST_node>* AST)
     {
         AST_node pre_process_questionmark;
         return pre_process_questionmark;
     }
-    inline AST_node Exclamationpoint()
+    inline AST_node Exclamationpoint(std::vector<AST_node>* AST)
     {
         AST_node pre_process_questionmark;
         return pre_process_questionmark;
     }
-    inline AST_node Single_Qoute() {
+    inline AST_node Single_Qoute(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Double_Qoute() {
+    inline AST_node Double_Qoute(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Left_Parenthesis() {
+    inline AST_node Left_Parenthesis(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Right_Parenthesis() {
+    inline AST_node Right_Parenthesis(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Left_Curlybracket() {
+    inline AST_node Left_Curlybracket(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Right_Curlybracket() {
+    inline AST_node Right_Curlybracket(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Left_Bracket() {
+    inline AST_node Left_Bracket(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Right_Bracket() {
+    inline AST_node Right_Bracket(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Line() {
+    inline AST_node Line(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Hash() {
+    inline AST_node Hash(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node At_Symbol() {
+    inline AST_node At_Symbol(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Dollar() {
+    inline AST_node Dollar(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Reference_Symbol() {
+    inline AST_node Reference_Symbol(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Procent_Symbol() {
+    inline AST_node Procent_Symbol(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node And_Symbol() {
+    inline AST_node And_Symbol(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Underline() {
+    inline AST_node Underline(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Back_Slash() {
+    inline AST_node Back_Slash(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Backtick() {
+    inline AST_node Backtick(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Circumflex() {
+    inline AST_node Circumflex(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Tilde() {
+    inline AST_node Tilde(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Section_Symbol() {
+    inline AST_node Section_Symbol(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Plus() {
+    inline AST_node Plus(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Minus() {
+    inline AST_node Minus(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Astrisk() {
+    inline AST_node Astrisk(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Forward_Slash() {
+    inline AST_node Forward_Slash(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Less_Than() {
+    inline AST_node Less_Than(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
-    inline AST_node Greater_Than() {
+    inline AST_node Greater_Than(std::vector<AST_node>* AST) {
         AST_node pre_process_seize;
         return pre_process_seize;
     }
 }
 
 namespace Complex_type {
-    inline AST_node Struct()
+    inline AST_node Struct(std::vector<AST_node>* AST)
     {
         AST_node pre_process_struct;
         return pre_process_struct;
     }
-    inline AST_node Class()
+    inline AST_node Class(std::vector<AST_node>* AST)
     {
         AST_node pre_process_class;
         return pre_process_class;
     }
-    inline AST_node Enum()
+    inline AST_node Enum(std::vector<AST_node>* AST)
     {
         AST_node pre_process_enum;
         return pre_process_enum;
@@ -382,32 +382,32 @@ namespace Complex_type {
 }
 
 namespace Function {
-    inline AST_node Main() {
+    inline AST_node Main(std::vector<AST_node>* AST) {
         AST_node pre_process_main;
         return pre_process_main;
     }
-    inline AST_node Function() {
+    inline AST_node Function(std::vector<AST_node>* AST) {
         AST_node pre_process_function;
         return pre_process_function;
     }
-    inline AST_node Return() {
+    inline AST_node Return(std::vector<AST_node>* AST) {
         AST_node pre_process_return;
         return pre_process_return;
     }
 }
 
 namespace Scope {
-    inline AST_node Using()
+    inline AST_node Using(std::vector<AST_node>* AST)
     {
         AST_node pre_process_using;
         return pre_process_using;
     }
-    inline AST_node Namespace()
+    inline AST_node Namespace(std::vector<AST_node>* AST)
     {
         AST_node pre_process_namespace;
         return pre_process_namespace;
     }
-    inline AST_node Enum()
+    inline AST_node Enum(std::vector<AST_node>* AST)
     {
         AST_node pre_process_enum;
         return pre_process_enum;
@@ -415,7 +415,7 @@ namespace Scope {
 }
 
 namespace Native_namespace {
-    inline AST_node Basic_Namespace()
+    inline AST_node Basic_Namespace(std::vector<AST_node>* AST)
     {
         AST_node pre_process_basic_namespace;
         return pre_process_basic_namespace;
@@ -423,19 +423,19 @@ namespace Native_namespace {
 }
 
 namespace Attribute {
-    inline AST_node Global() {
+    inline AST_node Global(std::vector<AST_node>* AST) {
         AST_node pre_process_global;
         return pre_process_global;
     }
-    inline AST_node Static() {
+    inline AST_node Static(std::vector<AST_node>* AST) {
         AST_node pre_process_static;
         return pre_process_static;
     }
-    inline AST_node Const() {
+    inline AST_node Const(std::vector<AST_node>* AST) {
         AST_node pre_process_const;
         return pre_process_const;
     }
-    inline AST_node Unsigned()
+    inline AST_node Unsigned(std::vector<AST_node>* AST)
     {
         AST_node pre_process_unsigned;
         return pre_process_unsigned;
